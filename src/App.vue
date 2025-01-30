c99bf816f18085fb32968cbc31d085d9ba3409d7
mnalley <mnalley95@gmail.com>
2025-01-30 06:56:06 -0800

    fix mobile button visibility

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ export default {
 }
 </script>
 
+
 <style>
 html,
 body,
@@ -25,18 +26,11 @@ body,
   width: 100%;
   height: 100%;
   overflow: hidden;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
 }
 
 #app {
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
 }
 </style>
